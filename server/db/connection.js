17ae5d6492bff1fdc8f67258cde460d4881e20e2
“NC_Legend” <59505853+6NCLegend9@users.noreply.github.com>
2025-04-16 09:35:41 -0500

    fnished with the code of the clone but there is there bugs

---
+++ server/db/connection.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+let db = null;
+
+export const ConnectDB = async (callback) => {
+  let dbName = "musicon";
+  try {
+    //MONGO_URL
+    let res = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    });
+
+    if (res) {
+      db = res.db(dbName);
+      callback(null, res);
+    }
+  } catch (err) {
+    callback(err, null);
+  }
+};
+
+export { db };
