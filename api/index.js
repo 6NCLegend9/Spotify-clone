77b34d825e6950fda46fb7f56a8bd97f438f25de
NC_Legend <telahon18@gmail.com>
2026-08-28 12:21:58 -0500

    Fix Vercel ESM API adapter

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-import app from "../server/app.js";
+let appPromise;
 
-export default app;
+function getApp() {
+	if (!appPromise) appPromise = import("../server/app.js").then((module) => module.default);
+	return appPromise;
+}
+
+module.exports = async (req, res) => {
+	const app = await getApp();
+	return app(req, res);
+};
